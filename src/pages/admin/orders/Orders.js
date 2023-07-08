23808eb5c50dfc287e10dd41efc7154305a5dc9d
khan8901 <khan890896@gmail>
2023-07-08 22:32:11 +0500

    hello

---
+++ src/pages/admin/orders/Orders.js
@@ -0,0 +1,99 @@
+import React, { useEffect } from "react";
+// import {
+//     allOrders,
+//     clearErrors,
+//     deleteOrder,
+// } from "../../../actions/orderActions";
+import Sidebar from "../../../components/admin/sidebar/Sidebar";
+import { useAlert } from "react-alert";
+import styles from "./Orders.module.scss";
+import Loader from "../../../components/loader/Loader";
+import { Table } from "react-bootstrap";
+import { AiOutlineDelete, AiOutlineEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import MetaData from "../../../components/MetaData";
+import orders from "../../../db/ordersDB";
+
+const Orders = ({ history }) => {
+  const alert = useAlert();
+  console.log("Orders is called.");
+
+  const isDeleted = false;
+  const loading = false;
+  const error = false;
+
+  useEffect(() => {
+    // dispatch(allOrders());
+
+    if (error) {
+      alert.error(error);
+      // dispatch(clearErrors());
+    }
+
+    if (isDeleted) {
+      alert.success("Order deleted successfully");
+      history.push("/admin/orders");
+    }
+  }, []);
+
+  const deleteOrderHandler = (id) => {
+    // dispatch(deleteOrder(id));
+  };
+
+  return (
+    <div className={styles.orders}>
+      <MetaData title={"Order"} />
+      <div className="row g-0">
+        <div className="col-md-2">
+          <Sidebar />
+        </div>
+        <div className="col-md-10">
+          <div className={`${styles.table} container mt-3`}>
+            {loading ? (
+              <>
+                <Loader />
+              </>
+            ) : (
+              <>
+                <Table responsive>
+                  <thead>
+                    <tr>
+                      <th>Order ID</th>
+                      <th>No of Items</th>
+                      <th>Amount</th>
+                      <th>Address</th>
+                      <th>Status</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    {orders?.map((order) => (
+                      <tr key={order?._id}>
+                        <td>{order?._id}</td>
+                        <td>{order.orderItems.length}</td>
+                        <td>{order?.totalPrice}</td>
+                        <td>{order?.shippingInfo.city}</td>
+                        <td>{order?.orderStatus}</td>
+                        <td className={styles.actions}>
+                          <Link to={`/admin/order/${order._id}`}>
+                            <AiOutlineEye size={20} />
+                          </Link>
+                          <button onClick={() => deleteOrderHandler(order._id)}>
+                            <AiOutlineDelete size={20} />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
---
+++ src/pages/auth/register/Register.js
@@ -0,0 +1,125 @@
+import React, { Fragment, useEffect, useState, useContext } from "react";
+import { useAlert } from "react-alert";
+import { Link } from "react-router-dom";
+import Footer from "../../../components/footer/Footer";
+import Navbar from "../../../components/header/Navbar";
+import ButtonLoader from "../../../components/loader/ButtonLoader";
+import MetaData from "../../../components/MetaData";
+import styles from "./Register.module.scss";
+import axios from "axios";
+import { baseUrl } from "../../../config";
+import { UserContext } from "../../../context/UserContext";
+
+const Register = ({ history, location }) => {
+  const { setUser, setToken, setAuthenticated } = useContext(UserContext);
+  const { token, user, isAuthenticated } = useContext(UserContext);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const alert = useAlert();
+
+  const error = false;
+  const loading = false;
+
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      history.push(redirect);
+    }
+
+    if (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const register = async () => {
+    try {
+      const data = {
+        name,
+        email,
+        password,
+      };
+
+      // const token = 'your-bearer-token';
+      const response = await axios.post(`${baseUrl}/register`, data);
+      // Handle the response
+       setUser(response.data.user); 
+       setToken(response.data.token); 
+       setAuthenticated(true); 
+
+
+
+    } catch (error) {
+      console.log(error);
+
+      // Handle the error
+    }
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Fragment>
+      <MetaData title={"Login"} />
+      <Navbar />
+      <div className={styles.login}>
+        <div className={styles.login_container}>
+          <h3 className="text-center text-white mb-3">Register</h3>
+          <form onSubmit={submitHandler}>
+            <div className={styles.from_group}>
+              <label htmlFor="name_field">Name</label>
+              <input
+                type="name"
+                placeholder="Enter your name ..."
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className={styles.from_group}>
+              <label htmlFor="email_field">Email</label>
+              <input
+                type="email"
+                placeholder="Enter your email ..."
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className={styles.from_group}>
+              <label htmlFor="password_field">Password</label>
+              <input
+                type="password"
+                placeholder="Enter your password ..."
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className={styles.from_group}>
+              <button
+                type="submit"
+                onClick={() => {
+                  register();
+                }}
+              >
+                {loading ? <ButtonLoader /> : "Register"}
+              </button>
+            </div>
+          </form>
+          <div className={styles.from_group}>
+            <p className="text-center text-white">
+              Already Have Account ? <Link to="/login">Signin</Link>
+            </p>
+            <p className="text-center text-white">
+              <Link to="/password/forgot">Forgot Password?</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Register;
+
